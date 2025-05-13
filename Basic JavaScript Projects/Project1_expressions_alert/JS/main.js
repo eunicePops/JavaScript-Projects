3590e97d285e68f18568dc0a3d0b22afa654b24b
Popoola Eunice Oluwadamilola <enablersolutions@gmail.com>
2025-05-13 13:29:35 -0400

    Basic JavaScript Projects

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+//A Basic JavaScript Program utilizing the window.alert() and document.write() methods 
+window.alert("Hello World Today!");
+document.write("Happy Mother's Day <br>")
+var A = "This is a string variable <br>";
+document.write(A);
+var courseTitle ="JavaScript";
+window.alert(courseTitle);
+var description =" Is JavaScript really the programming language of the web \? <br> Don\'t you think so...<br>";
+document.write(description);
+document.write("\"A great Day it is\","
+    + " says the toddler coming out of her classroom,"
+    +" That\'s awesome says Mom <br>"
+)
+var fullName = "My full name is: "+"Trent" + " Noah <br>"
+document.write(fullName)
+
+var courseOfStudy = "Software development", certificate = "Diploma", school = "Academy of Learning <br>";
+document.write(school);
+3 + 3;
+var Sent1 = "This is the begining of the string ", Sent2 = "and this is th end of the string "
+ document.write(Sent1 + Sent2);
+ 
